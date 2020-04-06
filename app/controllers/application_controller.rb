f9fa56b6a793903e502610720006356dde35dfd4
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-04-06 03:59:40 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def edit
+    @article = Article.find(params[:id])
   end
      
   def update
